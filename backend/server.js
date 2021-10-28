3d5868a1af92c9542adb03963f4dd831440be8d2
Artermis <nopphadon.phanwong@gmail.com>
2021-10-28 19:29:05 +0700

    Update Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/scheduDB/:id', (req, res) => {
+app.get('/', (req, res) => {
     res.send("Successful!")
 });
-app.get('/')
+
+
 
 app.listen(3000, () => console.log("Schedu app is listening on port 3000"))
