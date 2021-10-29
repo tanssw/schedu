a483cdf59fd0de9ba529d4d005344dc469ae7d5f
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-10-29 10:49:27 +0700

    Add more screen

---
+++ frontend/screens/appointment/AppointmentScreen.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function AppointmentScreen() {
+    return (
+        <Text>Hello, Appointment Screen!</Text>
+    )
+}
