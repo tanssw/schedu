6908cab510a1e46705c3477694798a82f2f5aadd
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-10-29 18:58:12 +0700

    Change stack name in calendar

--- frontend/navigators/CalendarNavigator.js
+++ frontend/navigators/CalendarNavigator.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import CalendarOverview from '../screens/calendar/CalendarOverviewScreen'
-import CalendarDetail from '../screens/calendar/CalendarDetailScreen'
+import CalendarOverviewScreen from '../screens/calendar/CalendarOverviewScreen'
+import CalendarDetailScreen from '../screens/calendar/CalendarDetailScreen'
+import AppointmentScreen from '../screens/appointment/AppointmentScreen'
 
-const ContactStack = createNativeStackNavigator()
+const CalendarStack = createNativeStackNavigator()
 
 export default function CalendarNavigator() {
     return (
-        <ContactStack.Navigator initialRouteName="CalendarOverview">
-            <ContactStack.Screen name="CalendarOverview" component={CalendarOverview} />
-            <ContactStack.Screen name="CalendarDetail" component={CalendarDetail} />
-        </ContactStack.Navigator>
+        <CalendarStack.Navigator initialRouteName="CalendarOverview">
+            <CalendarStack.Screen name="CalendarOverview" component={CalendarOverviewScreen} />
+            <CalendarStack.Screen name="CalendarDetail" component={CalendarDetailScreen} />
+            <CalendarStack.Screen name="Appointment" component={AppointmentScreen} />
+        </CalendarStack.Navigator>
     )
 }
