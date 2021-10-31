d0f1e3ccf8ec9382d77f2da0bf963f7eda1f10db
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-11-01 00:16:36 +0700

    Firebase Google Auth

--- frontend/App.js
+++ frontend/App.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { initializeApp } from 'firebase/app'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+import { firebaseConfig } from './config/firebase'
 
 import Navigator from './navigators/MainNavigator'
-
 import SignInScreen from './screens/account/SignInScreen'
 
-const isAuth = () => {
-    return false
-}
+initializeApp(firebaseConfig)
+const auth = getAuth()
 
 export default function App() {
+
+    const [userData, setUserData] = useState(null)
+
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            user ? setUserData(user) : setUserData(null)
+        })
+    })
+
     return (
         <>
-            { isAuth() ? <Navigator /> : <SignInScreen /> }
+            { userData ? <Navigator /> : <SignInScreen onPressSignIn={_signIn} /> }
         </>
     )
 }
