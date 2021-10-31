b6c94cd87cd911c1d6ffeadd75cdf69c84880b78
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-10-31 09:13:21 +0700

    Prepare conditionally rendering sign-in screen

--- frontend/App.js
+++ frontend/App.js
@@ -3,8 +3,16 @@ import { StyleSheet, Text, View } from 'react-native'
 
 import Navigator from './navigators/MainNavigator'
 
+import SignInScreen from './screens/account/SignInScreen'
+
+const isAuth = () => {
+    return false
+}
+
 export default function App() {
     return (
-        <Navigator />
+        <>
+            { isAuth() ? <Navigator /> : <SignInScreen /> }
+        </>
     )
 }
