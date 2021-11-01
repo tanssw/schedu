c6f64774a3fae81cc86db8be599857afd3a64b59
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-11-01 18:42:23 +0700

    Send userdata to state

--- frontend/App.js
+++ frontend/App.js
@@ -1,29 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { initializeApp } from 'firebase/app'
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import React, { useEffect, useState } from 'react'
+import { LogBox, StyleSheet, Text, View } from 'react-native'
+import Constants from 'expo-constants'
 
-import { firebaseConfig } from './config/firebase'
+import * as Google from 'expo-google-app-auth'
+// import { initializeApp } from 'firebase/app'
+// import { getAuth, onAuthStateChanged, signInWithCredential, signInWithCustomToken, GoogleAuthProvider } from 'firebase/auth'
+// import { firebaseConfig } from './config/firebase'
 
 import Navigator from './navigators/MainNavigator'
 import SignInScreen from './screens/account/SignInScreen'
 
-initializeApp(firebaseConfig)
-const auth = getAuth()
+// initializeApp(firebaseConfig)
+// const auth = getAuth()
+
+// Disable Yellow box warning
+LogBox.ignoreLogs(['AsyncStorage'])
 
 export default function App() {
 
     const [userData, setUserData] = useState(null)
 
-    useEffect(() => {
-        onAuthStateChanged(auth, user => {
-            user ? setUserData(user) : setUserData(null)
-        })
-    })
+    // useEffect(() => {
+    //     onAuthStateChanged(auth, user => {
+    //         console.log(user)
+    //         user ? setUserData(user) : setUserData(null)
+    //     })
+    // })
+
+    const signInWithGoogleAsync = async () => {
+        try {
+            const result = await Google.logInAsync({
+                iosClientId: Constants.manifest.extra.iosClientId,
+                scopes: ['profile', 'email']
+            })
+            if (result.type === 'success') {
+                setUserData(result.user)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <>
-            { userData ? <Navigator /> : <SignInScreen onPressSignIn={_signIn} /> }
+            { userData ? <Navigator /> : <SignInScreen onSignIn={signInWithGoogleAsync} /> }
         </>
     )
 }
--- frontend/screens/account/SignInScreen.js
+++ frontend/screens/account/SignInScreen.js
@@ -1,36 +1,19 @@
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import * as Google from 'expo-google-app-auth'
-import Constants from 'expo-constants'
+
 
 import { background, text } from '../../styles'
 
-const signInWithGoogleAsync = async () => {
-    try {
-        const result = await Google.logInAsync({
-            iosClientId: Constants.manifest.extra.iosClientId,
-            scopes: ['profile', 'email']
-        })
 
-        if (result.type === 'success') {
-            return result.accessToken
-        } else {
-            return { cancelled: true }
-        }
-    } catch (error) {
-        return { error: true }
-    }
 
-}
-
-export default function SignInScreen() {
+export default function SignInScreen(props) {
     return (
         <View style={[styles.container, background.blue]}>
             <View style={styles.appTitleContainer}>
                 <Text style={[styles.appTitle, text.lighterBlue]}>Sch</Text>
                 <Text style={styles.appTitle}>edu</Text>
             </View>
-            <TouchableOpacity onPress={signInWithGoogleAsync} style={styles.buttonSignIn}>
+            <TouchableOpacity onPress={props.onSignIn} style={styles.buttonSignIn}>
                 <Text style={[styles.textSignIn ,text.lightBlue]}>Sign in with ITKMITL Account</Text>
             </TouchableOpacity>
         </View>
