d311c320f292ba3b65761c255cac5f08967c38a3
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-11-04 14:02:26 +0700

    Mocked up my appointment

--- frontend/screens/calendar/components/MyAppointment.js
+++ frontend/screens/calendar/components/MyAppointment.js
@@ -1,12 +1,41 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { FontAwesome5 } from '@expo/vector-icons'
+import dayjs from 'dayjs'
 
-import { text } from '../../../styles'
+import { colorCode, text } from '../../../styles'
 
 export default function MyAppointment() {
 
-    const [requests, updateRequests] = useState([])
+    const [myAppointments, updateMyAppointments] = useState([
+        {id: 1, date: '2021-11-07', start: '01:00 PM', end: '01:45 PM', participant: ['Tasanai S', 'Suphakit K']},
+        {id: 2, date: '2021-11-07', start: '01:00 PM', end: '01:45 PM', participant: ['Tasanai S', 'Suphakit K']},
+        {id: 3, date: '2021-11-07', start: '01:00 PM', end: '01:45 PM', participant: ['Tasanai S', 'Suphakit K']},
+        {id: 4, date: '2021-11-07', start: '01:00 PM', end: '01:45 PM', participant: ['Tasanai S', 'Suphakit K']},
+    ])
+
+    const renderAppointment = (appointment) => {
+        return (
+            <View key={appointment.id} style={styles.appointmentItem}>
+                <View>
+                    <View style={styles.appointmentDesc}>
+                        <Text style={styles.appointmentDate}>{dayjs(appointment.date).format('DD MMM YYYY')}</Text>
+                        <Text style={styles.appointmentTime}>{appointment.start} - {appointment.end}</Text>
+                    </View>
+                    <Text style={styles.appointmentParticipant}>Participant: {appointment.participant.join(', ')}</Text>
+                </View>
+                <TouchableOpacity style={styles.viewButton}>
+                    <Text style={styles.viewButtonText}>View</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    const appointmentList = (
+        <View>
+            {myAppointments.map(appointment => renderAppointment(appointment))}
+        </View>
+    )
 
     const emptyRequest = (
         <View style={styles.emptyRequestContainer}>
@@ -18,7 +47,7 @@ export default function MyAppointment() {
     return (
         <View style={styles.container}>
             <Text style={styles.header}>My Appointments</Text>
-            {false ? '' : emptyRequest}
+            {true ? appointmentList : emptyRequest}
         </View>
     )
 }
@@ -43,5 +72,39 @@ const styles = StyleSheet.create({
     emptyRequestText: {
         marginVertical: 8,
         fontWeight: '300'
+    },
+    appointmentItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginVertical: 12
+    },
+    appointmentDesc: {
+        flexDirection: 'row'
+    },
+    appointmentDate: {
+        fontWeight: 'bold',
+        color: colorCode.blue,
+        marginRight: 8
+    },
+    appointmentTime: {
+        fontSize: 12,
+        alignSelf: 'center'
+    },
+    appointmentParticipant: {
+        fontSize: 12,
+        color: 'grey',
+        marginTop: 4
+    },
+    viewButton: {
+        borderWidth: 1,
+        borderColor: colorCode.blue,
+        borderRadius: 8,
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    viewButtonText: {
+        fontSize: 12,
+        color: colorCode.blue
     }
 })
