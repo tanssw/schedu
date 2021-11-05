9798ef0bdafc54f3971e4f42ef90704bf11c0db5
Artermis <nopphadon.phanwong@gmail.com>
2021-11-05 18:05:11 +0700

    Update component ContactTab

--- frontend/screens/contact/ContactListScreen.js
+++ frontend/screens/contact/ContactListScreen.js
@@ -1,86 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet,Text,View,TextInput,FlatList,TouchableOpacity} from "react-native";
-
-import {
-  AntDesign,
-  Fontisto,
-  FontAwesome,
-  Ionicons,
-} from "@expo/vector-icons";
+import React from "react";
+import {View} from "react-native";
 
 import SuggestBar from './components/SuggestBar'
 import SearchBar from './components/SearchBar'
 import QueryBar from "./components/QueryBar";
+import ContactTab from "./components/ContactTab";
 
 export default function ContactListScreen() {
 
-    const [participants, updateParticipants] = useState([
-        {
-          id: 1,
-          business_id: "62070074",
-          firstname: "Tasanai",
-          lastname: "Srisawat",
-          role: "Student"
-        },
-        {
-          id: 2,
-          business_id: "62070074",
-          firstname: "Tasanai",
-          lastname: "Srisawat",
-          role: "Student"
-        },
-        {
-          id: 3,
-          business_id: "62070074",
-          firstname: "Tasanai",
-          lastname: "Srisawat",
-          role: "Student"
-        },
-        {
-          id: 4,
-          business_id: "62070074",
-          firstname: "Tasanai",
-          lastname: "Srisawat",
-          role: "Student"
-        },
-        {
-            id: 5,
-            business_id: "62070074",
-            firstname: "Tasanai",
-            lastname: "Srisawat",
-            role: "Student"
-          },
-          {
-            id: 6,
-            business_id: "62070074",
-            firstname: "Tasanai",
-            lastname: "Srisawat",
-            role: "Student"
-          },
-          {
-            id: 7,
-            business_id: "62070074",
-            firstname: "Tasanai",
-            lastname: "Srisawat",
-            role: "Student"
-          },
-      ]);
-  const renderParticipantList = ({ item }) => {
-    return (
-      <View style={styles.listItem}>
-        <FontAwesome 
-          name="user-circle-o"
-          size={44}
-          color="grey"
-          style={styles.personImage}
-        />
-        <View>
-        <Text style={[styles.personName, styles.flex = 1]}>{item.firstname} {item.lastname}</Text>
-        <Text style={[styles.personRole]}>{item.role}</Text>
-        </View>
-      </View>
-    );
-  };
+  
   return (
     <View>
       {/* SearchBar tab*/}
@@ -90,55 +18,7 @@ export default function ContactListScreen() {
       {/* queryTab */}
         <QueryBar/>
       {/* contact tab */}
-      <View style={styles.ContactTab}>
-      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Contact</Text>
-      <View style={styles.listContainer}>
-          <FlatList
-            data={participants}
-            renderItem={renderParticipantList}
-            keyExtractor={(person) => person.id}
-            horizontal={false}
-            inverted={true}
-          />
-        </View>
-        </View> 
+      <ContactTab/>
     </View>
   );
-  function test() {
-    alert("history");
-  }
 }
-
-const styles = StyleSheet.create({
-  participantContainer: {
-    flexDirection: "row",
-    marginTop: 15,
-    marginBottom: 20,
-  },
-  personName: {
-    textAlign: "center",
-    marginTop: 4,
-  },
-  personRole:{
-    textAlign: 'left'
-  },
-  personImage: {
-    textAlign: "center",
-    marginVertical: 3,
-    marginHorizontal: 20,
-  },
-  ContactTab:{
-    margin: 15,
-  },
-  listContainer: {
-    // flexDirection: "row",
-    marginTop: 15,
-    marginBottom: 20,
-  },
-  listItem:{
-    flexDirection: 'row',
-    margin: 5,
-    marginBottom: 15,
-    alignSelf: 'flex-start'
-  },
-});
---
+++ frontend/screens/contact/components/ContactTab.js
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet,Text,View,TextInput,FlatList,TouchableOpacity} from "react-native";
+
+import {
+  AntDesign,
+  Fontisto,
+  FontAwesome,
+  Ionicons,
+} from "@expo/vector-icons";
+
+
+export default function ContactTab(){
+
+    const [participants, updateParticipants] = useState([
+        {
+          id: 1,
+          business_id: "62070074",
+          firstname: "Tasanai",
+          lastname: "Srisawat",
+          role: "Student"
+        },
+        {
+          id: 2,
+          business_id: "62070074",
+          firstname: "Tasanai",
+          lastname: "Srisawat",
+          role: "Student"
+        },
+        {
+          id: 3,
+          business_id: "62070074",
+          firstname: "Tasanai",
+          lastname: "Srisawat",
+          role: "Student"
+        },
+        {
+          id: 4,
+          business_id: "62070074",
+          firstname: "Tasanai",
+          lastname: "Srisawat",
+          role: "Student"
+        },
+        {
+            id: 5,
+            business_id: "62070074",
+            firstname: "Tasanai",
+            lastname: "Srisawat",
+            role: "Student"
+          },
+          {
+            id: 6,
+            business_id: "62070074",
+            firstname: "Tasanai",
+            lastname: "Srisawat",
+            role: "Student"
+          },
+          {
+            id: 7,
+            business_id: "62070074",
+            firstname: "Tasanai",
+            lastname: "Srisawat",
+            role: "Student"
+          },
+      ]);
+
+    const renderParticipantList = ({ item }) => {
+        return (
+          <View style={styles.listItem}>
+            <FontAwesome 
+              name="user-circle-o"
+              size={44}
+              color="grey"
+              style={styles.personImage}
+            />
+            <View>
+            <Text style={[styles.personName, styles.flex = 1]}>{item.firstname} {item.lastname}</Text>
+            <Text style={[styles.personRole]}>{item.role}</Text>
+            </View>
+          </View>
+        );
+      };
+
+    return(
+      <View style={styles.ContactTab}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Contact</Text>
+      <View style={styles.listContainer}>
+          <FlatList
+            data={participants}
+            renderItem={renderParticipantList}
+            keyExtractor={(person) => person.id}
+            horizontal={false}
+            inverted={true}
+          />
+        </View>
+        </View> 
+
+    )
+    
+        
+
+}
+const styles = StyleSheet.create({
+    participantContainer: {
+        flexDirection: "row",
+        marginTop: 15,
+        marginBottom: 20,
+      },
+      personName: {
+        textAlign: "center",
+        marginTop: 4,
+      },
+      personRole:{
+        textAlign: 'left'
+      },
+      personImage: {
+        textAlign: "center",
+        marginVertical: 3,
+        marginHorizontal: 20,
+      },
+      ContactTab:{
+        margin: 15,
+      },
+      listContainer: {
+        // flexDirection: "row",
+        marginTop: 15,
+        marginBottom: 20,
+      },
+      listItem:{
+        flexDirection: 'row',
+        margin: 5,
+        marginBottom: 15,
+        alignSelf: 'flex-start'
+      },
+})
