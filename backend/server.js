bdda9a64d3937fe3035df544926520a5149d121f
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2021-11-08 16:40:15 +0700

    Ordering line of codes in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,27 +19,27 @@ const user = require("./route/users")
 const appointment = require("./route/appointment")
 const notification = require("./route/notification")
 
-const app = express()
 const PORT = process.env.PORT
 
-// Logging incoming request into the console
-app.use(morgan('tiny'))
-
-// Initialize firebase application
+// Initialize Firebase Application
 initializeApp(firebaseConfig)
 
+// Initialize Express Application
+const app = express()
+app.use(morgan('tiny'))
 app.use(express.json())
 
+// Active Routers
 app.use("/appointment", appointment)
 app.use("/user", user)
 
+// Inactive Routers
 app.use("/student", student)
 app.use("/subject", subject)
 app.use("/teacher", teacher)
 app.use("/registrar", registrar)
 app.use("/teach", teach)
 app.use("/event", event)
-
 app.use("/notification", notification)
 
 app.get('/health', (req, res) => {
